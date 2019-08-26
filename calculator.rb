9c92dad2a15a479e54e5b11e0dec0a0f6d5dfac2
Jennifer Nicely <pipeline38@hotmail.com>
2019-08-26 23:24:46 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,9 @@
 # Add your variables here
 # Code your solution in calculator.rb
 
-first_number = 0 
-second_number = 1 
+first_number = 2   
+second_number = 1  
 sum = (first_number+second_number)
-
+difference = (first_number-second_number)
+product = (first_number*second_number)
+quotient = (first_number/second_number)
